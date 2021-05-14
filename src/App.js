748c173e01531235349f466b80c164bf2305b52c
woodylucas <wlucas0311@gmail.com>
2021-05-14 18:26:49 -0400

    finished refresh

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from "react";
 import "./App.css";
 
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clarke",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 class App extends Component {
   render() {
-    function formatName(user) {
-      return `${user.firstName} ${user.lastName}`;
-    }
-    const hellowWorld = "Welcome to the Raod to learn React";
-    const user = {
-      firstName: "Charlie",
-      lastName: "Brown",
-    };
-
     return (
       <div className="App">
-        <h2>{hellowWorld}</h2>
-        <h2>{formatName(user)}</h2>
+        {list.map((item) => {
+          return (
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>
+          );
+        })}
       </div>
     );
   }
